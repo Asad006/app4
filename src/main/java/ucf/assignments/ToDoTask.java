0ed29707830ee5ebd268e57d3aa5ee53580e5502
meroa006 <merouani.ass@gmail.com>
2021-07-05 12:06:16 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/ToDoTask.java
@@ -0,0 +1,16 @@
+package ucf.assignments;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Asad merouani
+ */
+
+public class ToDoTask {
+    /// toDoTitle : SimpleStringProperty
+
+    // Constructor
+    // ToDoTask(String toDoTitle)
+
+    // Methods
+    //public String getToDoTitle()
+    //public void setToDoTitle(String toDoTitle)
+}
